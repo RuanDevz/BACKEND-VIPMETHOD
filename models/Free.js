f2d165a25ead1a2685d3504add03a88a0c15247d
ruandevz <Ruanbatista1509@outlook.com>
2025-04-23 11:31:00 -0300

    category true

--- models/Free.js
+++ models/Free.js
@@ -34,7 +34,7 @@ module.exports = (sequelize, DataTypes) => {
     },
     category: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: true,
     },
     postDate: {
       type: DataTypes.DATE,
