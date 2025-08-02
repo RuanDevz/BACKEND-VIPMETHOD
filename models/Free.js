24d5b49a110e34ae01569f29527682ec653e98b8
ruandevz <Ruanbatista1509@outlook.com>
2025-08-02 19:37:43 -0300

    feat: impedir valor null nas tables

--- models/Free.js
+++ models/Free.js
@@ -32,7 +32,7 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: true,
     },
-      linkMV4:{
+    linkMV4:{
       type: DataTypes.STRING,
       allowNull: true,
     },
@@ -49,6 +49,16 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false,
       defaultValue: DataTypes.NOW,
     },
+  });
+
+  // Proteção contra apagar ou setar como null após definido
+  Free.beforeSave((instance, options) => {
+    if (instance.changed('link2') && instance.previous('link2') !== null && instance.link2 === null) {
+      instance.link2 = instance.previous('link2');
+    }
+    if (instance.changed('linkMV4') && instance.previous('linkMV4') !== null && instance.linkMV4 === null) {
+      instance.linkMV4 = instance.previous('linkMV4');
+    }
   });
 
   return Free;
